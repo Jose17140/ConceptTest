e56395657bc03b8bd8192bf781e6efea3aee4e78
Jose Bolivar <jose_boliv@hotmail.com>
2020-01-07 02:13:47 -0400

    Add sqlite, and library

---
+++ Mobile Apps/Mobile.Core/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Mobile.Services;
+using Mobile.Views;
+using Xamarin.Forms;
+
+namespace Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
