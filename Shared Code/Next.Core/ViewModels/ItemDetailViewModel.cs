5774c89db50895e34339bb3fcf68c45d71365ec8
Jose Bolivar <jose_boliv@hotmail.com>
2020-01-07 00:56:48 -0400

    Create Repository

---
+++ Shared Code/Next.Core/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Next.Models;
+
+namespace Next.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
