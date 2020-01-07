20362ec452b81ffb0e7c4818ede0abc68b34dd2c
Jose Bolivar <jose17140@hotmail.com>
2020-01-07 01:16:46 -0400

    Add file

--- Mobile Apps/Next.Droip/Properties/AssemblyInfo.cs
+++ Mobile Apps/Next.Droip/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Next.Android")]
+[assembly: AssemblyTitle("Next.Droip")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Next.Android")]
+[assembly: AssemblyProduct("Next.Droip")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
